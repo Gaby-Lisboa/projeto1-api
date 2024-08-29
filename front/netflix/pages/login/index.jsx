ecb5251a8ecb92f9ec3d5ce4c75d3960e6b8ea18
Gabrielly de Sousa Lisboa <52662441860@>
2024-08-29 11:27:50 -0300

    29/08

--- front/netflix/pages/login/index.jsx
+++ front/netflix/pages/login/index.jsx
@@ -1,31 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, TextInput, Button, Pressable } from 'react-native';
-import styles from './styles';
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, Pressable } from "react-native";
+import styles from "./styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 export default function Login() {
-  const [user, setUser] = useState('');
-  const [password, setPassword] = useState('');
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+    // const [,]=useState('')
+    const [token, setToken] = useState('')
 
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text>Usuário:</Text>
-        <TextInput
-          style={styles.caixa}
-          value={user}
-          onChangeText={(e) => {setUser(e)}}
-        />
-      </View>
-      <View style={{ marginTop: 20 }}> 
-        <Text>Senha:</Text>
-        <TextInput
-          style={styles.caixa}
-          value={password}
-          onChangeText={(e) => {setPassword(e)}}
-          secureTextEntry={true} 
-        />
-      </View>
-      {}
-    </View>
-  );
+    useEffect(() => {
+      AsyncStorage.setItem('token', token)
+        .then(
+          ()=>{
+              if (token != null){
+                console.log('Token Salvo:', token)
+              }
+
+          }
+        )
+        .catch (
+            (error)=>{
+              console.error('Erro ao salvar token:', error) 
+            }
+        )
+    })
+    const logar = async () => {
+      try {
+        const response = await axios.post(
+          'https://127.0.0.1:8000/api/token/',
+          {
+            username: user,
+            password: password,
+          }
+        )
+        console.log(response.data)
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.caixa}
+                value={user}
+                onChangeText={(e) => setUser(e)}
+                placeholder="User"
+            />
+            <TextInput
+                style={styles.caixa}
+                value={password}
+                onChangeText={(e) => setPassword(e)}
+                secureTextEntry={true}
+                placeholder="Password"
+            />
+            <Pressable
+                style={styles.btn}
+                
+            >
+                <Text>OK</Text>
+            </Pressable>
+
+
+        </View>
+    )
 }
--- front/netflix/pages/login/styles.jsx
+++ front/netflix/pages/login/styles.jsx
@@ -1,18 +1,32 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#ff0',
-      alignItems: 'center',
-      justifyContent: 'center',
+      alignItems:'center',
+      justifyContent:'center'
     },
     caixa:{
-        borderWidth:1,
-        borderRadius: 10
-    }
-   
-  });
-  
-  export default styles;
+      borderWidth:1,
+      borderRadius:10,
+      marginTop:20,
+      height:40,
+      width: '70%',
+      padding:10
+    },
+    btn: {
+      width: '20%',
+      height: 30,
+      backgroundColor: '#fcba03',
+      borderRadius: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderWidth: 1,
+      marginLeft: 5,
+      marginTop:20
+    },
+ 
+  })
+
+  export default styles
   
